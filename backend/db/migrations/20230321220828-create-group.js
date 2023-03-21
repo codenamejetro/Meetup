b715ce66ea37564c8cb3f0691a8f25a4c6adb1ac
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-03-21 15:37:46 -0700

    try 2

--- backend/db/migrations/20230321220828-create-group.js
+++ backend/db/migrations/20230321220828-create-group.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       organizerId: {
         type: Sequelize.INTEGER,
-        references: {model: "Users", key: 'id'},
+        references: {model: 'Users', key: 'id'},
         allowNull: false
       },
       name: {
