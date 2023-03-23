4c4bd5549b3d398b4c84e0e919bb5dd52722b824
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-03-23 16:25:40 -0700

    try again

--- backend/db/migrations/20230321220828-create-group.js
+++ backend/db/migrations/20230321220828-create-group.js
@@ -26,7 +26,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       type: {
-        type: Sequelize.ENUM(['in_person', 'online'])
+        type: Sequelize.ENUM('in_person', 'online')
       },
       private: {
         type: Sequelize.BOOLEAN
