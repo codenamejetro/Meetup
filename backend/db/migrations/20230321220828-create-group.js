59a28b81a2d4bd6568faa44fd43c70e8391f04ed
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-03-23 16:53:42 -0700

    test

--- backend/db/migrations/20230321220828-create-group.js
+++ backend/db/migrations/20230321220828-create-group.js
@@ -17,7 +17,7 @@ module.exports = {
       },
       organizerId: {
         type: Sequelize.INTEGER,
-        references: {model: 'Users', key: 'id'}
+        references: {model: 'User', key: 'id'}
       },
       name: {
         type: Sequelize.STRING
