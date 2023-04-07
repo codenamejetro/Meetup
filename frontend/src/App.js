3b202a7d0302b9e8e0181fd8fe35b9cf2ee74655
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-04-06 23:18:50 -0700

    Sign up form modal complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+import SignupFormModal from "./components/SignupFormModal";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+
 
 function App() {
   const dispatch = useDispatch();
@@ -18,7 +19,7 @@ function App() {
       {isLoaded && (
         <Switch>
           <Route path="/signup">
-            <SignupFormPage />
+            <SignupFormModal />
           </Route>
         </Switch>
       )}
