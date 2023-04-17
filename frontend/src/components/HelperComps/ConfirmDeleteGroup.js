012e8eb9e10b0858fb88d2238e3565612b74ef84
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-04-16 19:20:52 -0700

    styling

--- frontend/src/components/HelperComps/ConfirmDeleteGroup.js
+++ frontend/src/components/HelperComps/ConfirmDeleteGroup.js
@@ -26,9 +26,9 @@ function ConfirmDeleteGroup () {
 
 
     return (
-        <div>
-            <div className="confirm-title">Confirm Delete</div>
-            <div className="confirm-question">Are you sure you want to remove this group</div>
+        <div className="confirm-delete-wrapper">
+            <h1 className="confirm-title">Confirm Delete</h1>
+            <div className="confirm-question">Are you sure you want to remove this group?</div>
             <div className="confirm-delete confirm-buttons" onClick={deleteClick}>{`Yes (delete group)`}</div>
             <div className="confirm-keep confirm-buttons" onClick={keepClick}>{`No (keep group)`}</div>
         </div>
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -35,9 +35,10 @@ function SignupFormModal() {
         const data = await res.json();
         if (data && data.errors) {
           setErrors(data.errors);
-        }}
+        }
+      }
       ).then(closeModal())
-      ;
+        ;
     }
     return setErrors({
       confirmPassword: "Confirm Password field must be the same as the Password field"
@@ -46,69 +47,88 @@ function SignupFormModal() {
 
   return (
     <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          First Name
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
-        <label>
-          Last Name
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button disabled={!email || !username || username.length < 4 || !firstName || !lastName || !password || password.length < 6 || !confirmPassword || confirmPassword.length < 6} type="submit">Sign Up</button>
+      <h1 className="signup-modal-h1">Sign Up</h1>
+      <form className="signup-modal-form" onSubmit={handleSubmit}>
+        <div className="signup-modal-content">
+
+          <label className="signup-modal-labels">
+            <div>Email</div>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="signup-modal-inputs signup-modal-inputs-pairs"
+            />
+          </label>
+          {errors.email && <p>{errors.email}</p>}
+
+          <label className="signup-modal-labels">
+            <div>Username</div>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="signup-modal-inputs"
+            />
+          </label>
+          {errors.username && <p>{errors.username}</p>}
+
+          <label className="signup-modal-labels">
+            <div>First Name</div>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+              className="signup-modal-inputs signup-modal-inputs-pairs"
+            />
+          </label>
+          {errors.firstName && <p>{errors.firstName}</p>}
+
+          <label className="signup-modal-labels">
+            <div>Last Name</div>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+              className="signup-modal-inputs"
+            />
+          </label>
+          {errors.lastName && <p>{errors.lastName}</p>}
+
+          <label className="signup-modal-labels">
+            <div>Password</div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="signup-modal-inputs signup-modal-inputs-pairs"
+            />
+          </label>
+          {errors.password && <p>{errors.password}</p>}
+
+          <label className="signup-modal-labels">
+            <div>Confirm Password</div>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              className="signup-modal-inputs"
+              size={40}
+            />
+          </label>
+          {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+        </div>
+
+        <div className="signup-modal-button-div">
+
+        <button className="signup-modal-button"disabled={!email || !username || username.length < 4 || !firstName || !lastName || !password || password.length < 6 || !confirmPassword || confirmPassword.length < 6} type="submit">Sign Up</button>
+        </div>
       </form>
     </>
   );
